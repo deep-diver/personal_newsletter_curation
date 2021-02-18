e2c8e2fc3278ad8fa2aa3f5adf615111b56ff47c
deep-diver <deep.diver.csp@gmail.com>
2021-02-18 12:20:40 +0900

    add sample pipeline

---
+++ pipeline/Dockerfile
@@ -0,0 +1,4 @@
+FROM tensorflow/tfx:0.26.0
+WORKDIR /pipeline
+COPY ./ ./
+ENV PYTHONPATH="/pipeline:${PYTHONPATH}"
---
+++ pipeline/imdb_pipeline/.ipynb_checkpoints/bert-checkpoint.py
@@ -0,0 +1,9 @@
+
+import tensorflow_hub as hub
+
+BERT_TFHUB_URL = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3"
+                  
+def load_bert_layer(model_url=BERT_TFHUB_URL):
+    # Load the pre-trained BERT model as layer in Keras
+    bert_layer = hub.KerasLayer(handle=model_url, trainable=False)
+    return bert_layer
