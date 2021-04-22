26616837b902a164b16c487e2d86f4e8db0bc71e
chansung park <deep.diver.csp@gmail.com>
2021-04-23 00:02:27 +0900

    adding utility programs

---
+++ utils/convert_to_tfrecord.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import numpy as np
+import csv
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+"""
+structure reference from IMDB
+
+features {
+  feature {
+    key: "label"
+    value {
+      int64_list {
+        value: 0
+      }
+    }
+  }
+  feature {
+    key: "text"
+    value {
+      bytes_list {
+        value: "This was an absolutely terrible movie. Don\'t be lured in by Christopher Walken or Michael Ironside. Both are great actors, but this must simply be their worst role in history. Even their great acting could not redeem this movie\'s ridiculous storyline. This movie is an early nineties US propaganda piece. The most pathetic scenes were those when the Columbian rebels were making their cases for revolutions. Maria Conchita Alonso appeared phony, and her pseudo-love affair with Walken was nothing but a pathetic emotional plug in a movie that was devoid of any real meaning. I am disappointed that there are movies like this, ruining actor\'s like Christopher Walken\'s good name. I could barely sit through it."
+      }
+    }
+  }
+}
+
+"""
+
+def serialize_example(label, text):
+  """
+  Creates a tf.train.Example message ready to be written to a file.
+  """
+  # Create a dictionary mapping the feature name to the tf.train.Example-compatible
+  # data type.
+  feature = {
+      'label': _int64_feature(label),
+      'text': _bytes_feature(text)
+  }
+
+  # Create a Features message using tf.train.Example.
+
+  example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+  return example_proto.SerializeToString()
+
+def convert_csv_tfrecord(csv_path, tfrecord_path):
+   with open(csv_path, newline='') as csvfile:
+      csv_reader = csv.reader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE, escapechar='\\')
+
+      with tf.io.TFRecordWriter(tfrecord_path) as writer:
+         for row in csv_reader:
+            text = str.encode(row[0])
+            label = int(row[1])
+            example = serialize_example(label, text)
+            writer.write(example)
+
+# TEST
+serialized_example = serialize_example(0, b"hello world")
+example_proto = tf.train.Example.FromString(serialized_example)
+print(example_proto)
+
+print(convert_csv_tfrecord('./twitter.csv', './twitter.tfrecord'))
---
+++ utils/scrap_tweets.py
@@ -0,0 +1,58 @@
+import tweepy
+import re
+import csv
+
+def removeURLs(text):
+    return re.sub(r"http\S+", "", text)
+    
+def deEmojify(text):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'',text)
+
+csv.register_dialect('twitter_dialect', delimiter='|', quoting=csv.QUOTE_NONE, escapechar='\\')
+
+consumer_key=""
+consumer_secret=""
+
+access_token="-RLfv2XVHzfejeu8PKKayB2oUWNMNP2"
+access_token_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.user_timeline(user_id='algo_diver', count=1000, tweet_mode="extended")
+
+with open('twitter.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, dialect='twitter_dialect')
+#    writer.writerow(['text', 'category'])
+
+    for tweet in public_tweets:
+        if hasattr(tweet, 'retweeted_status'):
+            author = "@" + deEmojify(tweet.retweeted_status.author.name)
+            result = author + " " + deEmojify(tweet.retweeted_status.full_text.replace("\n", " ").replace("  ", " ").strip())
+            result = result.replace("|", "&")
+            result = removeURLs(result)
+            print("- " + result)
+            print("--------------------------------------------------------------")
+            writer.writerow([result, '0'])
